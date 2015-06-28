5931e55fb976c7899986520f474f7497a9590430
James Blanksby <james@thisismyengine.com>
2015-06-28 19:40:48 +0100

    Added config element descriptions

--- application/config/session.php
+++ application/config/session.php
@@ -2,7 +2,27 @@
 
 return array (
 
+	/*
+	|-----------------------------------------------------------------------
+	|	Session Name
+	|-----------------------------------------------------------------------
+    |
+    |	The session name references the name of the session, which is used 
+    |	in cookies and URLs.
+    |
+    */
+   
 	'name' => 'eixo',
-	'save_path' => 'application/session'
+
+	/*
+	|-----------------------------------------------------------------------
+	|	Session Save Path
+	|-----------------------------------------------------------------------
+    |
+    |	Sets the path where data for the current session is saved.
+    |
+    */
+
+	'save_path' => 'application/tmp/session'
 
 );
