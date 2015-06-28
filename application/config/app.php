af498edacfa395067eb634d30d145a4f1df536ed
James Blanksby <james@thisismyengine.com>
2015-06-28 19:29:41 +0100

    Added timezone description

--- application/config/app.php
+++ application/config/app.php
@@ -2,6 +2,16 @@
 
 return array (
 
+	/*
+	|-----------------------------------------------------------------------
+	|	Timezone
+	|-----------------------------------------------------------------------
+    |
+    |	Specify the default timezone for your application, which
+    |	will be used by the PHP date and date-time functions.
+    |
+    */
+   
 	'timezone' => 'Europe/London'
 
 );
